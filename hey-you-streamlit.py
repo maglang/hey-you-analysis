9590b62c63f8e70c6d2c682841e73af69d4b53af
maglang <angelie.maglasang@gmail.com>
2021-06-16 23:32:11 +0800

    'sample dashboard'

--- hey-you-streamlit.py
+++ hey-you-streamlit.py
@@ -1,4 +1,9 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+import os
+import seaborn as sns
+
+"""# EDA on Hey You Data"""
+
+st.selectbox('Objectives': (''))
